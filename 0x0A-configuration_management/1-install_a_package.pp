fc5506efd71166c05ec5cd035346a04eff5bf70b
talk2dennis <adigwedennis@gmail.com>
2024-03-24 22:28:15 +0100

    fixed the werkzeug version for flask version 2

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,12 @@
-#!/usr/bin/pup
 # puppet script to install flask using pip3
 package {'flask':
   ensure   => '2.1.0',
+  provider => 'pip3'
+}
+
+# install Werkzeug 2.1.1
+
+package {'werkzeug':
+  ensure   => '2.1.1',
   provider => 'pip3'
 }
