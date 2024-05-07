20c43c7f866b94d9d01854aedf4e54368e65bc18
talk2dennis <adigwedennis@yahoo.com>
2024-05-07 14:50:59 +0100

    completed task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+    A python script to retrieve the number of all subscribers of a subreddit
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Function to return the number of subscribers of a subreddit
+
+    Keyword arguments: subreddit (a string)
+    Return: return the number of subscribers of a subreddit
+    """
+    if not subreddit and not isinstance(subreddit, str):
+        return 0
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    headers = {
+        'User-Agent': 'myReddictBot/0.0.1'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        return 0
+    return response.json().get('data').get('subscribers')
