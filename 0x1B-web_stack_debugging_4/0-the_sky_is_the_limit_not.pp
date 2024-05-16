48cdf0276f23c26973a6c44f1ebdad591a90acfa
talk2dennis <adigwedennis@yahoo.com>
2024-05-16 21:30:46 +0100

    created a puppet file to configure nginx

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,13 @@
+# Using puppet to configure nginx to increase the trafic handled
+
+# Increase the ULIMIT of the default nginx config
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+} ->
+
+# Reload Nginx
+exec { 'nginx-restart':
+  command => 'nginx reload',
+  path    => '/etc/init.d/'
+}
